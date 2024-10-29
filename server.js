f92fc93654515be2b28ac9948c8ce716a4f42d36
Mohammed12des <hamood12des@gmail.com>
2024-10-29 16:19:07 +0300

    make new modifcation

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+// server.js
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const path = require("path"); // Add this line
+const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
@@ -32,6 +33,16 @@ app.use(
 );
 app.use(passUserToView);
 app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/download", isSignedIn, (req, res) => {
+  res.render("download.ejs");
+});
+
 app.get("/", (req, res) => {
   if (req.session.user) {
     res.redirect(`/users/${req.session.user._id}/rental`);
@@ -39,6 +50,7 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
   }
 });
+
 app.use("/auth", auth);
 app.use(isSignedIn);
 
